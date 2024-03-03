26bc38ccc958fec25aaab0bef67176524e1e7788
loki7349 <lokeshargon19l@gmail.com>
2024-03-03 13:54:36 +0530

    highlighted the button when you clicked

--- index.js
+++ index.js
@@ -9,3 +9,49 @@ let alignButtons = document.querySelectorAll(".align");
 let spacingButton = document.querySelectorAll(".spacing");
 let formatButtons = document.querySelectorAll(".format");
 let scriptButtons = document.querySelectorAll(".script");
+
+let fontList = [
+    "Arial", 
+    "Verdana", 
+    "Times New Roman",
+    "Garamond",
+    "Georgia",
+    "Courier New",
+    "cursive",
+];
+
+const initializer = () =>{
+    highlighter(alignButtons, true);
+    highlighter(spacingButton, true);
+    highlighter(formatButtons, false);
+    highlighter(scriptButtons, true);
+}
+
+const highlighter = (className, needsRemoval) => {
+    className.forEach((button) => {
+        button.addEventListener("click",()=> {
+            if(needsRemoval){
+                let alreadyActive = false;
+                if(button.classList.contains("active")){
+                    alreadyActive = true;
+                }
+    
+                highlighterRemover(className);
+                if(!alreadyActive){
+                    button.classList.add("active");
+                }
+            }
+            else{
+                button.classList.toggle("active");
+            }
+        });
+    });
+};
+
+const highlighterRemover = (className) => {
+    className.forEach((button) =>{
+        button.classList.remove("active");
+    })
+}
+
+window.onload = initializer();
